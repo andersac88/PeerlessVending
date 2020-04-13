8567896e4b5dc7696ac16425bbd7dc74c604729a
Andrew Andersen <andersac88@gmail.com>
2020-04-12 22:29:37 -0500

    map

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from "../components/Container"
+import Map from "../components/Map"
 
 function Services() {
   return (
   <>
   <Container />
   <Container />
+  <Map />
   <Container />
   </>
   )
