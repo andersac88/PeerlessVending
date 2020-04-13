03a5ea568d27f85e35cd37a07fdcb5de220ce684
Andrew Andersen <andersac88@gmail.com>
2020-04-12 19:17:43 -0500

    footer updates

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,11 +5,11 @@ function Footer() {
   return (
     <div id="footer" className="card-footer text-muted">
     <div className="row">
-      <div className="col-sm-8">
+      <div className="col-sm-6">
         <div className="row top">A percentage of all proceeds is donated to{'\u00A0'}<a href="https://www.thenccs.org/">The National Children's Cancer Society</a>
       </div>
      
-      <div className="row"> &#9400;Copyright 2020 by Peerless Vending | web design by AA</div>
+      <div className="row" id="copy"> &#9400;Copyright 2020 by Peerless Vending | web design by {'\u00A0'}<a  style={{color: "inherit"}} href="https://wwww.AndrewCAndersen.com">AA</a></div>
       </div>
       <div className="col-sm my-auto text-right">Connect with Us</div>
       <div className="col-sm my-auto">
