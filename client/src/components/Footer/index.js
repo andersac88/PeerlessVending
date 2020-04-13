32b98936911d0a3b15fbc389c2612a1416cb9ccc
Andrew Andersen <andersac88@gmail.com>
2020-04-12 22:34:48 -0500

    last update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,17 +6,17 @@ function Footer() {
     <div id="footer" className="card-footer text-muted">
     <div className="row">
       <div className="col-sm-6">
-        <div className="row top">A percentage of all proceeds is donated to{'\u00A0'}<a href="https://www.thenccs.org/">The National Children's Cancer Society</a>
+        <div className="row top">A percentage of all proceeds is donated to{'\u00A0'}<a target="_blank" rel="noopener noreferrer" href="https://www.thenccs.org/">The National Children's Cancer Society</a>
       </div>
      
-      <div className="row" id="copy"> &#9400;Copyright 2020 by Peerless Vending | web design by {'\u00A0'}<a  style={{color: "inherit"}} href="https://wwww.AndrewCAndersen.com">AA</a></div>
+      <div className="row" id="copy"> &#9400;Copyright 2020 by Peerless Vending | web design by {'\u00A0'}<a  style={{color: "inherit"}} target="_blank" rel="noopener noreferrer" href="https://www.AndrewCAndersen.com">AA</a></div>
       </div>
       <div className="col-sm-3"></div>
       <div className="col-sm my-auto text-right">Connect with Us</div>
       <div className="col-sm my-auto">
-        <a className="fab" href="https://www.facebook.com/PeerlessVending/" ><i className="fab fa-facebook-square"></i></a>
+        <a className="fab" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/PeerlessVending/" ><i className="fab fa-facebook-square"></i></a>
       {'\u00A0'}
-        <a className="fab" href="https://www.instagram.com/peerlessvending/"><i className="fab fa-instagram-square"></i></a>
+        <a className="fab" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/peerlessvending/"><i className="fab fa-instagram-square"></i></a>
       </div>
     </div>
     </div>
