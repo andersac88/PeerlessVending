bc52c83defaa782146558c49cfd178eb09f28888
Andrew Andersen <andersac88@gmail.com>
2020-04-28 19:25:15 -0500

    const for text

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,12 @@ import Map from "./components/Map"
 import axios from "axios"
 
 
+const top = "Peerless Vending is a family owned vending company based in Yorkville, Illinois that focuses on providing business owners with high quality vending machines for their employees and customers convenience."
+
+const middle = "Our services do not cost the business any money, as we place the machine, service and clean the machine, and fill the machine with the product. We offer a wide variety of candies for our machines at the request of the business or the customers of that business." 
+
+const bottom = "Providing and servicing vending machines around the Chicagoland area"
+
 class App extends Component {
   state = {
       name: '',
@@ -25,7 +31,7 @@ class App extends Component {
       })
     };
     
-    handleFormSubmit = async(event) => {
+    handleFormSubmit = (event) => {
       event.preventDefault();
       axios.post("/api/contact", this.state)
       .then(res =>{
@@ -38,9 +44,9 @@ class App extends Component {
     <Router>
       <div>
         <NavBar contactname={this.state.name} email={this.state.email} message={this.state.message} inputChange={this.handleInputChange} formSubmit={this.handleFormSubmit} />
-        <Containers side2="Peerless Vending is a family owned vending company based in Yorkville, Illinois that focuses on providing business owners with high quality vending machines for their employees and customers convenience." />
-        <Containers side1="Our services do not cost the business any money, as we place the machine, service and clean the machine, and fill the machine with the product. We offer a wide variety of candies for our machines at the request of the business or the customers of that business." />
-        <Map side1="Providing and servicing vending machines around the Chicagoland area" />
+        <Containers side2={top} />
+        <Containers side1={middle} />
+        <Map side1={bottom} />
         <Footer />
       </div>
     </Router>
